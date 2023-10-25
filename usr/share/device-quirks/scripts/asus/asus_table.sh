971528b963772f5ecbb4dc898453b06211253c5d
honjow <honjow311@gmail.com>
2023-10-25 12:55:51 +0800

    equalizer with ROG Ally

--- usr/share/device-quirks/scripts/asus/asus_table.sh
+++ usr/share/device-quirks/scripts/asus/asus_table.sh
@@ -7,7 +7,7 @@ CPU_NAME="$(cat /proc/cpuinfo | awk 'NR==5 {print $4$5$6$7}')"
 if [[ "ROG Ally RC71L_RC71L" == "$PRODUCT_NAME" ]] && [[ "RC71L" == "$BOARD_NAME" ]] && [[ "AMDRyzenZ1Extreme" == "$CPU_NAME" ]]; then
     echo "ROG Ally detected"
     $DQ_PATH/scripts/asus/ally/ally.sh
-    
+
 else
     echo "${PRODUCT_NAME} does not have a quirk configuration script. Exiting."
 fi
