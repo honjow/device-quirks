65def9b54c400051a95ebcd54ef9bedbf7773999
honjow <honjow311@gmail.com>
2023-11-18 01:20:48 +0800

    fix pipewire filename

--- usr/share/device-quirks/scripts/asus/ally/ally.sh
+++ usr/share/device-quirks/scripts/asus/ally/ally.sh
@@ -21,4 +21,4 @@ fi
 
 # Create an equalizer for pipewire.
 mkdir -p ~/.config/pipewire/pipewire.conf.d
-cp $DQ_PATH/scripts/asus/ally/sink-eq6.conf ~/.config/pipewire/pipewire.conf.d/sink-eq6.conf
+cp $DQ_PATH/scripts/asus/ally/sink-eq.conf /etc/pipewire/pipewire.conf.d/sink-eq.conf
