b55bd741d8f95ae95517d4192e0ce6cb5bca5d46
honjow <honjow311@gmail.com>
2024-03-06 01:19:19 +0800

    add ayanex next lite  modprobe iwlmvm on suspend

--- usr/lib/systemd/system-sleep/systemd-suspend-mods.sh
+++ usr/lib/systemd/system-sleep/systemd-suspend-mods.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # This file runs during sleep/resume events. It will read the list of modules
-# in /etc/device-quirks/systemd-suspend-mods.conf and rmmod them on suspend,
-# insmod them on resume.
+# in /etc/device-quirks/systemd-suspend-mods.conf and modprobe -r them on suspend,
+# modprobe them on resume.
+
+MOD_LIST=$(grep -v ^\# /etc/device-quirks/systemd-suspend-mods.conf)
 
 PRODUCT=$(cat /sys/devices/virtual/dmi/id/product_name)
 ROG_LIST="ROG Ally RC71L_RC71L:ROG Ally RC71L"
+NEXT_LIST="NEXT Advance:NEXT Lite:NEXT Pro:NEXT"
 
 SKIP_MT7921E=false
 if [[ ":$ROG_LIST:" =~ ":$PRODUCT:" ]]; then
     SKIP_MT7921E=true
 fi
 
-MOD_LIST=$(grep -v ^\# /etc/device-quirks/systemd-suspend-mods.conf)
+if [[ ":$NEXT_LIST:" =~ ":$PRODUCT:" ]]; then
+    MOD_LIST+=('iwlmvm')
+fi
+
+echo -e "MOD_LIST: \n$MOD_LIST"
 
 case $1 in
     pre)
