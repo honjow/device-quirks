a104b9c2f340197ac99fd7c0cbf8ca48fbe39cb9
honjow <honjow311@gmail.com>
2024-03-05 13:38:04 +0800

    ROG Ally skip modprobe mt7921e it will cause suspend exception

--- usr/lib/systemd/system-sleep/systemd-suspend-mods.sh
+++ usr/lib/systemd/system-sleep/systemd-suspend-mods.sh
@@ -3,16 +3,30 @@
 # in /etc/device-quirks/systemd-suspend-mods.conf and rmmod them on suspend,
 # insmod them on resume.
 
+PRODUCT=$(cat /sys/devices/virtual/dmi/id/product_name)
+ROG_LIST="ROG Ally RC71L_RC71L:ROG Ally RC71L"
+
+SKIP_MT7921E=false
+if [[ ":$ROG_LIST:" =~ ":$PRODUCT:" ]]; then
+    SKIP_MT7921E=true
+fi
+
 MOD_LIST=$(grep -v ^\# /etc/device-quirks/systemd-suspend-mods.conf)
 
 case $1 in
     pre)
         for mod in $MOD_LIST; do
+            if [[ $SKIP_MT7921E == true && $mod == "mt7921e" ]]; then
+                continue
+            fi
             modprobe -r $mod
         done
     ;;
     post)
         for mod in $MOD_LIST; do
+            if [[ $SKIP_MT7921E == true && $mod == "mt7921e" ]]; then
+                continue
+            fi
             modprobe $mod
         done
     ;;
