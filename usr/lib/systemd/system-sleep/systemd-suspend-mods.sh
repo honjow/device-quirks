ba3631a45a5b0738839e07708700fc66f83c6eac
honjow <honjow311@gmail.com>
2024-04-17 16:31:19 +0800

    add AYANEO_2021_LIST

--- usr/lib/systemd/system-sleep/systemd-suspend-mods.sh
+++ usr/lib/systemd/system-sleep/systemd-suspend-mods.sh
@@ -10,6 +10,7 @@ PRODUCT=$(cat /sys/devices/virtual/dmi/id/product_name)
 ROG_LIST="ROG Ally RC71L_RC71L:ROG Ally RC71L"
 NEXT_LIST="NEXT Advance:NEXT Lite:NEXT Pro:NEXT"
 GEEK_LIST="GEEK:GEEK 1S"
+AYANEO_2021_LIST="AYA NEO FOUNDER:AYA NEO 2021:AYANEO 2021:AYANEO 2021 Pro:AYANEO 2021 Pro Retro Power"
 
 SKIP_MT7921E=false
 if [[ ":$ROG_LIST:" =~ ":$PRODUCT:" ]]; then
@@ -22,7 +23,7 @@ if [[ ":$NEXT_LIST:" =~ ":$PRODUCT:" ]]; then
     MOD_LIST=$(echo -e "${MOD_LIST}\niwlmvm")
 fi
 
-if [[ ":$GEEK_LIST:" =~ ":$PRODUCT:" ]]; then
+if [[ ":$GEEK_LIST:" =~ ":$PRODUCT:" || ":$AYANEO_2021_LIST:" =~ ":$PRODUCT:"  ]]; then
     echo "GEEK detected, adding goodix_ts to the list of modules to be removed"
     MOD_LIST=$(echo -e "${MOD_LIST}\ngoodix_ts")
 fi
