a932e66715444ebf5b0a508c204d5283c2885371
honjow <honjow311@gmail.com>
2024-06-24 02:26:49 +0800

    add more device to disable speaker power saving

--- usr/share/device-quirks/scripts/awinic/enable_speaker.sh
+++ usr/share/device-quirks/scripts/awinic/enable_speaker.sh
@@ -3,9 +3,9 @@ echo aw87xxx_pa 0x5b > /sys/bus/i2c/devices/i2c-1/new_device
 
 
 PRODUCT_NAME=$(cat /sys/devices/virtual/dmi/id/product_name)
-PRODUCT_LIST_AIR_1S="AIR 1S:AIR 1S Limited"
+PRODUCT_LIST="AIR 1S:AIR 1S Limited:FLIP DS:FLIP KB:KUN:AIR Plus"
 
-if [[ ":$PRODUCT_LIST_AIR_1S:" =~ ":$PRODUCT_NAME:" ]]; then
-    echo "AYANEO AIR 1S detected, disabling speaker power saving"
+if [[ ":$PRODUCT_LIST:" =~ ":$PRODUCT_NAME:" ]]; then
+    echo "ayaneo detected, disabling speaker power saving"
 	echo 0 | tee /sys/module/snd_hda_intel/parameters/power_save
 fi
