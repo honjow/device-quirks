7bb4db95058501d5f92b1815bb40ead96f85c96a
honjow <honjow311@gmail.com>
2025-02-11 13:39:13 +0800

    update set headroom for claw8

--- usr/share/device-quirks/scripts/msi/claw8/claw8.sh
+++ usr/share/device-quirks/scripts/msi/claw8/claw8.sh
@@ -14,23 +14,24 @@ else
   source /etc/device-quirks/device-quirks.conf
 fi
 
-ALSA_CONF="$DQ_PATH/scripts/msi/claw8/media-session.d/alsa-monitor.conf"
-PIPEWIRE_DIR="${MOUNT_PATH}/etc/pipewire/media-session.d/"
+# ALSA_CONF="$DQ_PATH/scripts/msi/claw8/media-session.d/alsa-monitor.conf"
+# PIPEWIRE_DIR="${MOUNT_PATH}/etc/pipewire/media-session.d/"
 
-# Check if the PipeWire directory exists
-if [[ -f "${ALSA_CONF}" ]]; then
-  echo "Installing pipewire config from ${ALSA_CONF} to ${PIPEWIRE_DIR}"
-  if [[ ! -d "${PIPEWIRE_DIR}" ]]; then
-    mkdir -p "${PIPEWIRE_DIR}"
-  fi
+# # Check if the PipeWire directory exists
+# if [[ -f "${ALSA_CONF}" ]]; then
+#   echo "Installing pipewire config from ${ALSA_CONF} to ${PIPEWIRE_DIR}"
+#   if [[ ! -d "${PIPEWIRE_DIR}" ]]; then
+#     mkdir -p "${PIPEWIRE_DIR}"
+#   fi
 
-  # Copy the PipeWire folder to /etc
-  cp "${ALSA_CONF}" "${PIPEWIRE_DIR}"
-  echo "PipeWire configuration successfully copied to ${PIPEWIRE_DIR}"
-fi
+#   # Copy the PipeWire folder to /etc
+#   cp "${ALSA_CONF}" "${PIPEWIRE_DIR}"
+#   echo "PipeWire configuration successfully copied to ${PIPEWIRE_DIR}"
+# fi
 
 # Force 16 bit audio, format S16LE, sample rate 96000.
 # echo "Force S16LE 96000hz"
 # $DQ_PATH/scripts/override_bitrate
 
-sed -i 's/--\["api.alsa.headroom"\]      = 1024/\["api.alsa.headroom"\]      = 2048/' ${MOUNT_PATH}/etc/wireplumber/main.lua.d/50-alsa-config.lua
+cp -a ${SUBVOL}/usr/share/wireplumber ${MOUNT_PATH}/etc/
+sed -i 's/--\["api.alsa.headroom"\]      = 1024/\["api.alsa.headroom"\]      = 4096/' ${MOUNT_PATH}/etc/wireplumber/main.lua.d/50-alsa-config.lua
