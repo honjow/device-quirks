adc76aafc86a0edc94d56414bcb2fe1099f6516f
honjow <honjow311@gmail.com>
2025-04-29 20:21:09 +0800

    feat: add support for Claw A1M with configuration and script

---
+++ usr/share/device-quirks/scripts/msi/claw_a1m/claw_a1m.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+# Check for root
+if [ "$(whoami)" != 'root' ]; then
+  echo "You must be root to run this script."
+  exit 1
+fi
+
+if [ -d /tmp/frzr_root ]; then
+  source ${MOUNT_PATH}/etc/device-quirks/device-quirks.conf
+else
+  source /etc/device-quirks/device-quirks.conf
+fi
+
+WIREPLUMBER_DST="${MOUNT_PATH}/etc/wireplumber/wireplumber.conf.d"
+WIREPLUMBER_SRC="$DQ_PATH/scripts/msi/claw_a1m/wireplumber.conf.d"
+
+# wireplumber_config.sh
+$DQ_PATH/scripts/wireplumber_config.sh "$WIREPLUMBER_SRC" "$WIREPLUMBER_DST"
+
+# modules-load.d
+MODULES_LOAD_D_DST="${MOUNT_PATH}/etc/modules-load.d/"
+MODULES_LOAD_D_SRC="$DQ_PATH/scripts/msi/claw_a1m/modules-load.d"
+mkdir -p "$MODULES_LOAD_D_DST"
+for file in $(find "$MODULES_LOAD_D_SRC" -type f); do
+  filename=$(basename "$file")
+  dst="$MODULES_LOAD_D_DST/$filename"
+  cp "$file" "$dst"
+done
--- usr/share/device-quirks/scripts/msi/msi_table.sh
+++ usr/share/device-quirks/scripts/msi/msi_table.sh
@@ -5,6 +5,9 @@ BOARD_NAME="$(cat /sys/devices/virtual/dmi/id/board_name)"
 # B650I EDGE WIFI
 BOARD_B650I_EDGE="MPG B650I EDGE WIFI (MS-7D73)"
 
+# Claw A1M
+BOARD_CLAW_A1M="MS-1T41"
+
 # Claw 8
 BOARD_CLAW8="MS-1T52"
 
@@ -15,6 +18,10 @@ if [[ ":$BOARD_B650I_EDGE:" =~ ":$BOARD_NAME:" ]]; then
 elif [[ ":$BOARD_CLAW8:" =~ ":$BOARD_NAME:" ]]; then
   echo "Claw 8"
   $DQ_PATH/scripts/msi/claw8/claw8.sh
+
+elif [[ ":$BOARD_CLAW_A1M:" =~ ":$BOARD_NAME:" ]]; then
+  echo "Claw A1M"
+  $DQ_PATH/scripts/msi/claw_a1m/claw_a1m.sh
 
 # No Match
 else
