558f5145a21503c2f3ff97f2f227e1474972cd17
honjow <honjow311@gmail.com>
2025-05-03 05:27:35 +0800

    feat: add kernel option for pcie_aspm to improve suspend/resume

--- usr/share/device-quirks/scripts/msi/claw_a1m/claw_a1m.sh
+++ usr/share/device-quirks/scripts/msi/claw_a1m/claw_a1m.sh
@@ -29,3 +29,6 @@ for file in $(find "$MODULES_LOAD_D_SRC" -type f); do
   dst="$MODULES_LOAD_D_DST/$filename"
   cp "$file" "$dst"
 done
+
+echo "Adding pcie_aspm=force to kernel options, to fix suspend/resume"
+$DQ_PATH/scripts/kernel-options-manager --append pcie_aspm=force
