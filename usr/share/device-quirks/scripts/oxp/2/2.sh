8be5693342c049b53aa8977ab50f850b101c1e3d
honjow <honjow311@gmail.com>
2025-11-28 19:27:17 +0800

    refactor: simplify service enabling and starting logic in audio fix scripts for OXP devices

--- usr/share/device-quirks/scripts/oxp/2/2.sh
+++ usr/share/device-quirks/scripts/oxp/2/2.sh
@@ -8,14 +8,8 @@ if [ "$(whoami)" != 'root' ]; then
   exit 1
 fi
 
-IS_ENABLED=$(systemctl is-enabled oxp2p-audio-fix.service)
-if [ "$IS_ENABLED" != "enabled" ]; then
-  echo "Enabling oxp2p-audio-fix.service"
-  systemctl enable oxp2p-audio-fix.service >/dev/null 2>&1
-fi
+echo "Enabling oxp2p-audio-fix.service"
+systemctl enable oxp2p-audio-fix.service >/dev/null 2>&1 || true
 
-IS_ACTIVE=$(systemctl is-active oxp2p-audio-fix.service)
-if [ "$IS_ACTIVE" != "active" ]; then
-  echo "Starting oxp2p-audio-fix.service"
-  systemctl start oxp2p-audio-fix.service >/dev/null 2>&1
-fi
+echo "Starting oxp2p-audio-fix.service"
+systemctl start oxp2p-audio-fix.service >/dev/null 2>&1 || true
